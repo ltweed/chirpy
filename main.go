9ef812db2bd72e44871071005fdad667b02db803
Larry <larry@lltweed.com>
2024-12-31 14:56:00 -0800

    switched to provided sol cuz it's gooder

--- main.go
+++ main.go
@@ -6,22 +6,26 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	// Use http.FileServer to serve files from the current directory
-	fileServer := http.FileServer(http.Dir("."))
-
-	// Handle the root path ("/") and serve the index.html file
-	mux.Handle("/", fileServer)
-	
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 
